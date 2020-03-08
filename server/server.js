56e226359920d6e890d0b20d51311fdf519b4dfa
alibek <sstlegion277@gmail.com>
2020-03-08 22:49:18 +0700

    working chat

--- server/server.js
+++ server/server.js
@@ -3,11 +3,37 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const nunjucks = require('nunjucks');
+const server = require('http').Server(app);
+const io = require('socket.io')(server, {serveClient: true});
 
-app.get('/', (request, response) => {
-   response.sendFile(path.join(__dirname, '..', 'client', 'index.html'));
+nunjucks.configure('./client/view', {
+    autoescape: true,
+    express: app
 });
 
-app.listen(3000,'0.0.0.0', () => {
+app.use('/assets', express.static('./client/public'));
+
+app.get('/', (req, res) => {
+    res.render('index.html', {date: new Date() });
+});
+
+io.on('connection', function (socket) {
+    socket.emit('connected', "You are connected");
+    socket.join('all');
+
+    socket.on('msg', content => {
+        console.log("MSG", content);
+        const obj = {
+            date: new Date(),
+            content: content,
+            username: socket.id
+        };
+        socket.emit("message", obj);
+        socket.to('all').emit("message", obj);
+    })
+});
+
+server.listen(3000,'0.0.0.0', () => {
     console.log('server started on port 3000');
 });
