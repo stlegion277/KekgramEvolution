f06862d38cecd1c68c2203b6d0c7034259760886
alibek <sstlegion277@gmail.com>
2020-03-12 22:44:01 +0700

    messages history and mongodb is done. authorization don't work yet(

--- server/server.js
+++ server/server.js
@@ -6,6 +6,22 @@ const path = require('path');
 const nunjucks = require('nunjucks');
 const server = require('http').Server(app);
 const io = require('socket.io')(server, {serveClient: true});
+const mongoose = require('mongoose');
+const passport = require('passport');
+const {Strategy, ExtractJwt} = require('passport-jwt');
+
+const opts = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: 'rCSVbzHGct9sZuBsRV8SHAA'
+};
+
+passport.use(new Strategy(opts, function(jwt_payload, done) {
+    if(jwt_payload != void(0)) return done(false, jwt_payload);
+    done();
+}));
+
+mongoose.connect('mongodb://localhost:27017/chatik', {});
+mongoose.Promise = require('bluebird');
 
 nunjucks.configure('./client/view', {
     autoescape: true,
@@ -14,25 +30,21 @@ nunjucks.configure('./client/view', {
 
 app.use('/assets', express.static('./client/public'));
 
-app.get('/', (req, res) => {
+function checkAuth (req, res, next) {
+    passport.authenticate('jwt', {session:false}, (err, decryptToken, jwtError) => {
+        if (jwtError != void(0) || err != void(0)) return res.render('index.html', {error: err || jwtError });
+        req.user = decryptToken;
+        next();
+    })
+}
+
+app.get('/', checkAuth, (req, res) => {
     res.render('index.html', {date: new Date() });
 });
 
-io.on('connection', function (socket) {
-    socket.emit('connected', "You are connected");
-    socket.join('all');
+require('./sockets')(io);
 
-    socket.on('msg', content => {
-        console.log("MSG", content);
-        const obj = {
-            date: new Date(),
-            content: content,
-            username: socket.id
-        };
-        socket.emit("message", obj);
-        socket.to('all').emit("message", obj);
-    })
-});
+
 
 server.listen(3000,'0.0.0.0', () => {
     console.log('server started on port 3000');
